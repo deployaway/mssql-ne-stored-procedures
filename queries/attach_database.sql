a795b283beb4ca1733e7ed2c3fe07aa5771d6f04
deployaway <136957622+deployaway@users.noreply.github.com>
2024-01-10 18:56:18 -0500

    Added queries

---
+++ queries/attach_database.sql
@@ -0,0 +1,10 @@
+CREATE DATABASE StackOverflow
+    ON (FILENAME = '/sqlserverdata/StackOverflow_1.mdf'),
+    (FILENAME = '/sqlserverdata/StackOverflow_2.ndf'),
+    (FILENAME = '/sqlserverdata/StackOverflow_3.ndf'),
+    (FILENAME = '/sqlserverdata/StackOverflow_4.ndf'),
+    (FILENAME = '/sqlserverdata/StackOverflow_log.ldf')
+FOR ATTACH;
+
+
+SELECT * FROM [sys].[dm_tcp_listener_states]
